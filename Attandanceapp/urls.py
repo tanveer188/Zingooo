9cad381b8cd381c1c82e2661ca950f746c49a2e5
Tanveer hussain <64352429+tanveer188@users.noreply.github.com>
2023-02-21 23:16:43 +0530

    Update urls.py

--- Attandanceapp/urls.py
+++ Attandanceapp/urls.py
@@ -3,6 +3,7 @@ from django.urls import path,include
 from Attandanceapp import views
 
 urlpatterns = [
+    path('', views.login,name="LoginPage"),
     path('login', views.login,name="LoginPage"),
     path('Attandance',views.submit)
 ]
