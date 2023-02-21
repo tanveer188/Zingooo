38bb6da72b84a633c790c614fe621a77853661fb
Tanveer hussain <64352429+tanveer188@users.noreply.github.com>
2023-02-21 17:34:20 +0530

    Update settings.py

--- zingooo/settings.py
+++ zingooo/settings.py
@@ -132,7 +132,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #]
 
 STATIC_URL = '/static/'
-if DEBUG:
-  STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-else:
-  STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
